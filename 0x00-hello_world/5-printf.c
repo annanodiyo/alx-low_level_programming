08304d7b432ca701ca1b5af9043ed742736b8c55
annanodiyo <annanodiyo@gmail.com>
2023-06-17 15:29:05 -0700

    printing using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints using printf
- * Return: 0
+ * main - prints using printf function
+ * return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
