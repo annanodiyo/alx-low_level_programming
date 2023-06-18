34a176630e4869e55814168a89a083aab3bfd8f6
annanodiyo <annanodiyo@gmail.com>
2023-06-18 05:27:28 -0700

    size of vasrious computer sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long int s;
 	float t;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(p));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(q));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(r));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(s));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(t));
+printf("Size of a char: %d byte(s)\n", (unsigned long)sizeof(p));
+printf("Size of an int: %d byte(s)\n", (unsigned long)sizeof(q));
+printf("Size of a long long int: %d byte(s)\n", (unsigned long)sizeof(r));
+printf("Size of a long int: %d byte(s)\n", (unsigned long)sizeof(s));
+printf("Size of a float: %d byte(s)\n", (unsigned long)sizeof(t));
 return (0);
 }
