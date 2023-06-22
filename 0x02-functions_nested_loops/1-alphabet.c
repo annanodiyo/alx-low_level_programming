6c1bfeed027ee3ad8da20ce179441112e4881b64
annanodiyo <annanodiyo@gmail.com>
2023-06-21 18:34:23 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet using putchar
+ *
+ * Return: 0 always on success
+ */
+
+void print_alphabet(void)
+{
+char r;
+for (r = 'a'; r <= 'z'; r++)
+_putchar(r);
+_putchar('\n');
+}
+
