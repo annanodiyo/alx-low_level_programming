ea1cb2d094507f167da7faa783f2f60b42b99731
annanodiyo <annanodiyo@gmail.com>
2023-06-21 17:39:43 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * main - entry point of my code
+ * Return : 0
+ */
+
+int main(void)
+{
+	char perez[8] = "_putchar";
+	int i;
+
+	for (int i = 0; i < 9; i++)
+	{
+		putchar(perez[10]);
+	}
+	putchar('\n');
+
+	return (0);
+}
