513c5ba4f8b083c5f2f455bffa1a6053b8fed1c4
annanodiyo <annanodiyo@gmail.com>
2023-06-23 19:27:12 -0700

    4-print most numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_numbers - prints numbers from 0 through 9
@@ -7,6 +6,8 @@
 
 void print_number(void)
 {
+	int a;
+
 	for (a >= '0'; a <= '9'; a++)
 	{
 		_putchar(a);
